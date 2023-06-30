4839fab8002abdcea17eb0f33e2bd6d919bc5159
Jack Kwok <jack.kwok@concept4.net>
2023-06-30 18:34:18 +0800

    lesson 12 - unfinished

--- exercise-12-materials/readme.md
+++ exercise-12-materials/readme.md
@@ -14,3 +14,22 @@ npm run dev
 # Build for production in the dist/ directory
 npm run build
 ```
+
+Materials
+    put a color on each visible pixel of the geometries.
+    algorithms that decide on the color of each pixel are written in programs called shaders
+
+MeshBasicMaterial
+    applies a uniform color or a texture on our geometry
+    map property will apply a texture on the surface
+    color property will apply a uniform color on the surface of the geometry. 
+
+AlphaMao
+    control opacity of mesh with transparent true
+
+MeshNormalMaterial
+    Usage of Normals:
+        how to illuminate the face
+        how the environment should reflect on the geometries' surface.
+        how the environment refract on the geometries' surface.
+    
--- exercise-12-materials/src/script.js
+++ exercise-12-materials/src/script.js
@@ -12,6 +12,49 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Textures
+const textureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+// Meshes
+const basicMaterial = new THREE.MeshBasicMaterial({
+    map: doorColorTexture,
+    color: 0xff00ff,
+    // wireframe: true,
+    transparent: true,
+    // alphaMap: doorAlphaTexture,
+    side: THREE.DoubleSide
+});
+const normalMaterial = new THREE.MeshNormalMaterial({
+
+})
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5,16,16),
+  basicMaterial
+);
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1,1),
+  basicMaterial
+);
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  basicMaterial
+);
+
+sphere.position.x = - 1.5
+torus.position.x = 1.5
+scene.add(sphere, plane, torus)
+
+
 /**
  * Sizes
  */
@@ -70,6 +113,17 @@ const tick = () =>
 
     // Update controls
     controls.update()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+    camera.lookAt(plane.position)
+
 
     // Render
     renderer.render(scene, camera)
