4cab8400999e072b840488f4f1cb9ade9604754b
Jack Kwok <jack.kwok@concept4.net>
2023-06-30 14:40:35 +0800

    lesson 10 learnt how to use a gui to make changes in my parameter

--- exercise-10-debug-ui/src/script.js
+++ exercise-10-debug-ui/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'lil-gui'
 
 /**
  * Base
@@ -14,10 +15,30 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+const parameters = {
+    color: 0xff0000,
+    spin: () => {
+        mesh.rotation.y += Math.PI / 4;
+        renderer.render(scene, camera);
+    }
+}
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01).name('elevation')
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+
+gui.addColor(parameters, 'color').onChange(() => {
+    material.color.set(parameters.color) })
+gui.add(parameters, 'spin')
 
 /**
  * Sizes
