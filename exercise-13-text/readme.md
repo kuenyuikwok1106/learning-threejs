c8ec7ba6ecdc32dd6eea957e342730f22f5c20b9
Jack Kwok <jack.kwok@concept4.net>
2023-07-16 21:10:32 +0800

    lesson 13 learnt how to render a text with its material and geometry

--- exercise-13-text/readme.md
+++ exercise-13-text/readme.md
@@ -14,3 +14,25 @@ npm run dev
 # Build for production in the dist/ directory
 npm run build
 ```
+
+Three.js already supported text geometry.
+But we need font family file
+
+How to get font file
+1. convert using https://gero3.github.io/facetype.js/
+2. find fonts in the Three.js examples located in the /node_modules/three/examples/fonts/
+    and import to static/fonts folder
+
+
+How to show text?
+1. use FontLoader
+2. load the font in onSuccess callback function and add it to the scene
+
+Creating a text geometry is long and hard for the computer.
+keep the geometry as low poly as possible
+    reduce the curveSegments and bevelSegments properties.
+
+Centering Text
+the TextGeometry is actually wrapped by a sphere by default.
+turn the wrapper to be a box by calling computeBoundingBox()
+translate using textGeometry.translate(x, y, z) || textGeometry.center() to center it
--- exercise-13-text/src/script.js
+++ exercise-13-text/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import * as dat from 'lil-gui'
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 THREE.ColorManagement.enabled = false
 
@@ -17,41 +19,26 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-
-scene.add(cube)
-
-/**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
 window.addEventListener('resize', () =>
 {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -69,10 +56,46 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /**
+ * Fonts
+ */
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+  '/fonts/helvetiker_regular.typeface.json',
+  (font) => {
+    const textGeometry = new TextGeometry(
+      'Hello Three.js',
+      {
+        font: font,
+        size: 0.5,
+        height: 0.2,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 5
+      }
+    )
+    const textMaterial = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture,
+      // wireframe: true,
+    })
+    const text = new THREE.Mesh(textGeometry, textMaterial)
+    textGeometry.computeBoundingBox()
+    console.log(textGeometry.boundingBox)
+    textGeometry.center()
+    scene.add(text)
+  }
+)
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
@@ -85,16 +108,30 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+for(let i = 0; i < 100; i++) {
+  const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+  donut.position.x = (Math.random() - 0.5) * 10
+  donut.position.y = (Math.random() - 0.5) * 10
+  donut.position.z = (Math.random() - 0.5) * 10
+  donut.rotation.x = Math.random() * Math.PI
+  donut.rotation.y = Math.random() * Math.PI
+  const scale = Math.random()
+  donut.scale.set(scale, scale, scale)
+  scene.add(donut)
 }
 
 tick()
